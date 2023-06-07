c074485bbd8e4e155cf305b8ff214ca1ae457e5a
ALTA-FE14-Robbi <robbiromadhoni@gmail.com>
2023-06-07 22:13:50 +0700

    login dengan fake API

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,14 +4,16 @@ interface ButtonProps {
   id: string;
   label?: string;
   onClick?: React.MouseEventHandler;
+  disabled?: boolean;
 }
 
-const Button: FC<ButtonProps> = ({ id, label, onClick }) => {
+const Button: FC<ButtonProps> = ({ id, label, onClick, disabled }) => {
   return (
     <button
       id={id}
       className="btn bg-alta-blue hover:bg-alta-orange text-white shadow-lg w-3/5 rounded-3xl m-5"
       onClick={onClick}
+      disabled={disabled}
     >
       {label}
     </button>
--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -4,16 +4,33 @@ interface FormLoginProps {
   id: string;
   label?: string;
   type?: any;
+  name?: string;
+  value?: string | number;
+  error?: string | boolean | undefined;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const FormLogin: FC<FormLoginProps> = ({ id, label, type }) => {
+const FormLogin: FC<FormLoginProps> = ({
+  id,
+  label,
+  type,
+  name,
+  value,
+  error,
+  onChange,
+}) => {
   return (
     <div>
       <input
+        className={`input input-bordered bg-white shadow-lg w-3/5 rounded-3xl m-1 ${
+          error && "border-red-600"
+        }`}
         id={id}
         type={type}
+        name={name}
         placeholder={label}
-        className="input input-bordered bg-white shadow-lg w-3/5 rounded-3xl m-1"
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
